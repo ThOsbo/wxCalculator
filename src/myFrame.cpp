d5c9dd4bbee133c498b3f19a22c7345729a713f1
Spirit1230 <tom.osborn1@btinternet.com>
2021-08-04 17:09:58 +0100

    Changed font

--- src/myFrame.cpp
+++ src/myFrame.cpp
@@ -15,15 +15,21 @@ MyFrame :: MyFrame() : wxFrame(NULL, wxID_ANY, "Calculator", wxPoint(10, 10), wx
     wxGridSizer *numbersSizer = new wxGridSizer(4, 3, 5, 5);
     wxGridSizer *opsSizer = new wxGridSizer(2, 2, 5, 5);
 
+    wxFont font(25, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+
     displayWindow = new wxTextCtrl(this, wxID_ANY, "", wxPoint(10, 10), wxSize(X_MAIN_FRAME_DIMENSION, 50), wxTE_READONLY);
+    displayWindow -> SetFont(font);
 
     displaySizer -> Add(displayWindow, 0, wxEXPAND | wxALL, 5);
 
     for (int i = 1; i <= 10; i++) 
     {
         wxButton *numButton = new wxButton(this, 2000 + i, std :: to_string(i % 10), wxPoint(0, 0), wxSize(40, 40));
+        numButton -> SetFont(font);
+        numButton -> Bind(wxEVT_COMMAND_BUTTON_CLICKED, MyFrame :: OnNumButtonClick, this);
+
         numbersSizer -> Add(numButton, 1, wxEXPAND | wxALL, 2.5); 
-        numButton -> Bind(wxEVT_COMMAND_BUTTON_CLICKED, MyFrame :: OnNumButtonClick, this);              
+                      
     }
 
     controlSizer -> Add(numbersSizer, 1, wxEXPAND | wxALL, 5);
@@ -33,6 +39,8 @@ MyFrame :: MyFrame() : wxFrame(NULL, wxID_ANY, "Calculator", wxPoint(10, 10), wx
     for (int i = 0; i < 4; i++) 
     {
         wxButton *opButton = new wxButton(this, wxID_ANY, operations[i], wxPoint(0, 0), wxSize(40, 40));
+        opButton -> SetFont(font);
+
         opsSizer -> Add(opButton, 1, wxEXPAND | wxALL, 2.5);
     }
 
