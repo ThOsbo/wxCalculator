1cedece60707ff9ea7157ef081e6f440b80d3a78
Spirit1230 <tom.osborn1@btinternet.com>
2021-08-11 11:13:46 +0100

    Prepared for performing calculations

--- include/calculate.h
+++ include/calculate.h
@@ -2,15 +2,19 @@
     #define CALCULATE_H
 
     #include <string>
+    #include <regex>
 
     class PerformCalculation
     {
         public :
-            PerformCalculation();
+            static std :: string GetResult(std :: string toCalc);
 
-        private :
-            std :: string toCalc = "";
-
+        private : 
+            static std :: string EvaluateExpression(std :: string expression);
+            static std :: string Add(int num1, int num2);
+            static std :: string Minus(int num1, int num2);
+            static std :: string Multiply(int num1, int num2);
+            static std :: string Divide(int num1, int num2);
     };
 
 #endif
--- src/calculate.cpp
+++ src/calculate.cpp
@@ -1,1 +1,36 @@
 #include <calculate.h>
+
+std :: string PerformCalculation :: GetResult(std :: string toCalc) 
+{
+    std :: regex bracReg("\\([^\\(\\)]+\\)");
+    toCalc = std :: regex_replace(toCalc, bracReg, "[$&]");
+
+    return EvaluateExpression(toCalc);
+}
+
+std :: string PerformCalculation :: EvaluateExpression(std :: string expression) 
+{
+    std :: regex numReg("\\d+(\\.\\d+)?");
+
+    return std :: regex_replace(expression, numReg, "[$&]");
+}
+
+std :: string PerformCalculation :: Add(int num1, int num2) 
+{
+    return std :: to_string(num1 + num2);
+}
+
+std :: string PerformCalculation :: Minus(int num1, int num2) 
+{
+    return std :: to_string(num1 - num2);
+}
+
+std :: string PerformCalculation :: Multiply(int num1, int num2) 
+{
+    return std :: to_string(num1 * num2);
+}
+
+std :: string PerformCalculation :: Divide(int num1, int num2) 
+{
+    return std :: to_string(num1 / num2);
+}
